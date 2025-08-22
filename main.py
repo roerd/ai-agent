fb9b141165eb7400d86ea6cd294d0a5b7ca8631f
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-22 20:15:23 +0200

    Add verbose flag

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from google.genai import types
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <prompt>")
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <prompt> [--verbose]")
         sys.exit(1)
 
     user_prompt = sys.argv[1]
+    verbose = len(sys.argv) > 2 and sys.argv[2] == "--verbose"
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -22,13 +23,15 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-
     response = client.models.generate_content(model=model, contents=messages)
-    print(response.text)
 
-    usage_metadata = response.usage_metadata
-    print("Prompt tokens:", usage_metadata.prompt_token_count)
-    print("Response tokens:", usage_metadata.candidates_token_count)
+    if verbose:
+        print("User prompt:", user_prompt)
+        usage_metadata = response.usage_metadata
+        print("Prompt tokens:", usage_metadata.prompt_token_count)
+        print("Response tokens:", usage_metadata.candidates_token_count)
+
+    print("Response:", response.text)
 
 
 if __name__ == "__main__":
