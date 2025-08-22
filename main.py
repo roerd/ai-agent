e041ec819c98bd45a66a38ae244ee7b81d192b3c
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-22 20:09:05 +0200

    Use prompt from command line argument

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import os
-from idlelib.pyshell import usage_msg
+import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <prompt>")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
 
-    response = client.models.generate_content(
-        model=model,
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(model=model, contents=messages)
     print(response.text)
 
     usage_metadata = response.usage_metadata
