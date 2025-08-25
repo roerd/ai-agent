7564973d5716d7240c62de9e48792568d1c16f22
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-25 22:52:57 +0200

    Use strict mode on Path.resolve()

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,15 +5,15 @@ from config import max_file_size
 
 def get_file_content(working_directory, file_path):
     wd_path = Path(working_directory)
-    full_path = (wd_path / file_path).resolve()
-
-    if not full_path.is_relative_to(wd_path.resolve()):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-
-    if not full_path.is_file():
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-
     try:
+        full_path = (wd_path / file_path).resolve(strict=True)
+
+        if not full_path.is_relative_to(wd_path.resolve(strict=True)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not full_path.is_file():
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
         content = full_path.read_text()
         if len(content) > max_file_size:
             content = content[:max_file_size] + f'[...File "{file_path}" truncated at 10000 characters]'
