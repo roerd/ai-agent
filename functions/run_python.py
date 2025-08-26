edcd4583c243a19c1462b4e0e93476df80174098
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-27 00:19:03 +0200

    Implement run_python_file

---
+++ functions/run_python.py
@@ -0,0 +1,31 @@
+import subprocess
+from pathlib import Path
+
+
+def run_python_file(working_directory, file_path, args=()):
+    wd_path = Path(working_directory)
+    full_path = (wd_path / file_path).resolve()
+
+    if not full_path.is_relative_to(wd_path.resolve()):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not full_path.exists():
+        return f'Error: File "{file_path}" not found.'
+
+    if not full_path.suffix == '.py':
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        completed_process = subprocess.run([str(full_path)] + args, cwd=wd_path, capture_output=True, timeout=30)
+        output = []
+        if completed_process.stdout:
+            output.append(f'STDOUT:{completed_process.stdout}')
+        if completed_process.stderr:
+            output.append(f'STDERR:{completed_process.stderr}')
+        if not output:
+            output.append("No output produced.")
+        if completed_process.returncode != 0:
+            output.append(f'Process exited with code {completed_process.returncode}')
+        return '\n'.join(output)
+    except subprocess.SubprocessError as e:
+        return f"Error: executing Python file: {e}"
