cefe88e3384a8a1ed9dfb9b3839279afd5918531
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-26 14:15:09 +0200

    Remove resolve with strict mode again from get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,15 +5,15 @@ from config import MAX_FILE_SIZE
 
 def get_file_content(working_directory, file_path):
     wd_path = Path(working_directory)
+    full_path = (wd_path / file_path).resolve()
+
+    if not full_path.is_relative_to(wd_path.resolve()):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not full_path.is_file():
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
     try:
-        full_path = (wd_path / file_path).resolve(strict=True)
-
-        if not full_path.is_relative_to(wd_path.resolve(strict=True)):
-            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-
-        if not full_path.is_file():
-            return f'Error: File not found or is not a regular file: "{file_path}"'
-
         content = full_path.read_text()
         if len(content) > MAX_FILE_SIZE:
             content = content[:MAX_FILE_SIZE] + f'[...File "{file_path}" truncated at 10000 characters]'
