cfeb51580424052614479f004b07f49beda79e7a
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-26 14:00:36 +0200

    Implement write_file

---
+++ functions/write_file.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def write_file(working_directory, file_path, content):
+    wd_path = Path(working_directory)
+    try:
+        full_path = (wd_path / file_path).resolve(strict=True)
+
+        if not full_path.is_relative_to(wd_path.resolve(strict=True)):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+        # This will either create a new file or overwrite an existing file, depending on whether it already exists.
+        full_path.write_text(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except OSError as e:
+        return f'Error: {e}'
