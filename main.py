6609c7251aeba91789d4162542731432b6b17553
Rörd Hinrichsen <roerd.hinrichsen@uni-hamburg.de>
2025-08-29 01:19:38 +0200

    Implement call_function

--- main.py
+++ main.py
@@ -5,10 +5,50 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from functions.get_file_content import schema_get_file_content
-from functions.get_files_info import schema_get_files_info
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
+
+
+def call_function(function_call_part: types.FunctionCall, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    functions = {
+        'get_file_content': get_file_content,
+        'get_files_info': get_files_info,
+        'run_python_file': run_python_file,
+        'write_file': write_file,
+    }
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+
+    try:
+        function = functions[function_name]
+        function_result = function(working_directory='calculator', **function_args)
+
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    except KeyError:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
 
 
 def main():
@@ -67,7 +107,11 @@ def main():
 
     print("Response:", response.text)
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose=verbose)
+
+        response = function_call_result.parts[0].function_response.response
+        if verbose:
+            print(f"-> {response}")
 
 
 if __name__ == "__main__":
